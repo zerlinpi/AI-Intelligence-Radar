544e65722bc3a0c43efe05e2b3eb676437ecc5d1
zerlinpi <105773292+zerlinpi@users.noreply.github.com>
2026-08-25 11:47:10 +0800

    refactor: improve unified logger configuration

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,14 +1,22 @@
 import logging
+import sys
 
 
-def get_logger(name: str):
+
+def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
-    if not logger.handlers:
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            '%(asctime)s | %(levelname)s | %(name)s | %(message)s'
-        )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(logging.INFO)
+
+    if logger.handlers:
+        return logger
+
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
     return logger
